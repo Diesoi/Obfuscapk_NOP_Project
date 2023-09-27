02ccd8a767f7f9b1cb01b99c6641107d4ae1f1bb
Diego Soi <diegosoi1@hotmail.it>
2023-09-27 09:55:30 +0200

    Update select_subsamples.py

--- src/select_subsamples.py
+++ src/select_subsamples.py
@@ -1,5 +1,7 @@
 import os
 import random
+import json
+
 
 def create_subset(data_dir):
 
@@ -32,10 +34,48 @@ def read_subset(data_dir):
     return list_apk_subset
 
 
+def select_year(data_dir):
+
+    list_apk_subset = read_subset(data_dir)
+    with open(os.path.join(data_dir, "Benign.json")) as fp:
+        benign_year = json.load(fp)
+    with open(os.path.join(data_dir, "Malicious.json")) as fp:
+        malicious_year = json.load(fp)
+
+    # print(benign_year, malicious_year)
+    counter = {str(k): 0 for k in range(2008, 2023)}
+
+    i = 1
+    """
+    for type in list_apk_subset.keys():
+        for apk in list_apk_subset[type]:
+            print(i)
+            try:
+                if type == 'Benign':
+                    counter[benign_year[apk]] += 1
+                else:
+                    counter[malicious_year[apk]] += 1
+                i += 1
+            except:
+                pass
+    """
+
+    for type in list_apk_subset.keys():
+        for apk in os.listdir(os.path.join(data_dir, type)):
+            try:
+                if type == 'Benign':
+                    counter[benign_year[apk.replace(".apk", "")]] += 1
+                else:
+                    counter[malicious_year[apk.replace(".apk", "")]] += 1
+            except:
+                pass
+    print(counter)
+
+
 if __name__ == '__main__':
 
     data_dir = os.path.join("..", "..", "Data", "Set")
 
     # create_subset(data_dir)
     list_apk_subset = read_subset(data_dir)
-    print(list_apk_subset)
+    select_year(data_dir)
